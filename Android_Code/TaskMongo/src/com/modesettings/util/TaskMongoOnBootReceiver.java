1e3789bd3dd78a9690e57e6c195515f520a943c5
Minakshi Bhatnagar <misha311289@yahoo.in>
2016-01-07 16:43:40 +0530

    Implemented Boot Receiver to resume rules on phone restart

---
+++ Android_Code/TaskMongo/src/com/modesettings/util/TaskMongoOnBootReceiver.java
@@ -0,0 +1,36 @@
+package com.modesettings.util;
+
+import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+ 
+/**
+ * BroadCastReceiver for android.intent.action.BOOT_COMPLETED
+ * passes all responsibility to TaskButlerService.
+ *
+ */
+
+public class TaskMongoOnBootReceiver extends BroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+    	
+    	try{
+    		Toast.makeText(context, "Starting TaskMongo Rules...", Toast.LENGTH_LONG).show();
+    	Util.refreshAllAlarms(context);
+    	}catch(Exception e){
+    		AlertDialog.Builder alert = new AlertDialog.Builder(context);
+    		alert.setTitle("Exception");
+    		alert.setMessage(e.getMessage());
+    		alert.setPositiveButton("OK", null);
+    		alert.show();
+    	}catch (Error e) {
+    		AlertDialog.Builder alert = new AlertDialog.Builder(context);
+    		alert.setTitle("Error");
+    		alert.setMessage(e.getMessage());
+    		alert.setPositiveButton("OK", null);
+    		alert.show();
+		}
+    }
+}
